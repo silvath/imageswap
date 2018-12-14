b97e6e02117af1ce0e9a98d4b9b4fe544361b37a
Thiago Henrique da Silva <thsilva@gmail.com>
2018-12-14 18:14:05 -0200

    create project

---
+++ src/WebImageswap/Controllers/ImageController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using WebImageswap.Services;
+
+namespace WebImageswap.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class ImageController : Controller
+    {
+        private ImageService _image;
+        public ImageController(ImageService image)
+        {
+            _image = image;
+        }
+        //[HttpGet]
+        //[ProducesResponseType(typeof(IEnumerable<GameInfo>), (int)HttpStatusCode.OK)]
+        //[ProducesResponseType(typeof(Exception), (int)HttpStatusCode.InternalServerError)]
+        //public async Task<ActionResult> GetGames()
+        //{
+        //    try
+        //    {
+        //        return(Ok(await _crawler.Search(null)));
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return (StatusCode((int)HttpStatusCode.InternalServerError, e));
+        //    }
+        //}
+    }
+}
---
+++ src/WebImageswap/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using Swashbuckle.AspNetCore.Swagger;
+using Sysphera.Middleware.Drapo;
+using Microsoft.Net.Http.Headers;
+using WebImageswap.Services;
+
+namespace WebGwg
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDrapo();
+            services.AddMvc()
+                  .AddJsonOptions(options =>
+                  {
+                      options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                  });
+            services.AddSingleton<ImageService, ImageService>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "Imageswap",
+                    Version = "v1",
+                    Description = "API to be used in Imageswap",
+                    TermsOfService = "None"
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseDrapo(o => { ConfigureDrapo(env, o); });
+            app.UseDefaultFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                OnPrepareResponse = (context) =>
+                {
+                    var headers = context.Context.Response.GetTypedHeaders();
+                    headers.CacheControl = new CacheControlHeaderValue() { NoCache = true };
+                }
+            });
+            app.UseAuthentication();
+            app.UseMvcWithDefaultRoute();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebSPA API");
+            });
+        }
+
+        private void ConfigureDrapo(IHostingEnvironment env, DrapoMiddlewareOptions options)
+        {
+            if (env.IsDevelopment())
+                options.Debug = true;
+            options.Config.UsePipes = false;
+            options.Config.StorageErrors = "errors";
+            options.Config.OnError = "";
+        }
+    }
+}
